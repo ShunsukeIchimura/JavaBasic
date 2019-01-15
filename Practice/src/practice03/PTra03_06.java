88d6ee8a3bd08f72784a40b003dd2ca162f74434
ShunsukeIchimura <46698049+ShunsukeIchimura@users.noreply.github.com>
2019-01-15 17:46:46 +0900

    3-9

--- Practice/src/practice03/PTra03_06.java
+++ Practice/src/practice03/PTra03_06.java
@@ -25,6 +25,13 @@ public class PTra03_06 {
 		 *
 		 * ※ プログラムは何行書いても良いです
 		 */
-
+		System.out.println(random);
+		if(random >100 || random <= 30) {
+			System.out.println("☆☆☆");
+		}else if(random <= 70) {
+			System.out.println("☆☆☆☆");
+		}else if(random <= 100) {
+			System.out.println("☆☆☆☆☆");
+		}
 	}
 }
