7fc1bba86392ff2432308c8e3b9ae37188cfad8e
ShunsukeIchimura <46698049+ShunsukeIchimura@users.noreply.github.com>
2019-01-15 13:00:09 +0900

    1-7

--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -21,24 +21,24 @@ public class PTra01_07 {
 
 		// ★ それぞれの型変数に、値を代入してください
 		// ※ 値は、好きな数字を入力してください
-
-
-
-
-
-
-
-
+		bo = true;
+		by = 2;
+		s = 12;
+		c = 'ま';
+		i = 21;
+		l = 314159265L;
+		f = 3.14F;
+		d = 3.141592653589793238462643383279;
 
 		// ★ それぞれの変数の中身を出力してください
-
-
-
-
-
-
-
-
+		System.out.println(bo);
+		System.out.println(by);
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
 
 	}
 }
